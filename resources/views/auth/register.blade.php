2734220cbd7903a3132ee906f5f88ce2995bc9bd
Steve Tom <tomsteve187@gmail.com>
2023-09-18 12:26:49 +0300

    Fixed Navigation changes and auth controller (#1)
    * Updated README.md
    
    * Fixed Registration Functionality

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,51 @@
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
+        <!-- Registration Number -->
+        <div class="mt-4">
+            <x-input-label for="reg_no" :value="__('Reg. No')" />
+            <x-text-input id="reg_no" class="block mt-1 w-full" type="text" name="reg_no" :value="old('reg_no')" required />
+            <x-input-error :messages="$errors->get('reg_no')" class="mt-2" />
+        </div>
+
+        <!-- ID Number -->
+        <div class="mt-4">
+            <x-input-label for="id_no" :value="__('ID No')" />
+            <x-text-input id="id_no" class="block mt-1 w-full" type="text" name="id_no" :value="old('id_no')" required />
+            <x-input-error :messages="$errors->get('id_no')" class="mt-2" />
+        </div>
+
+        <!-- Gender -->
+        <div class="mt-4">
+            <x-input-label for="gender" :value="__('Gender')" />
+            <x-select class="" id="gender" name="gender">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </x-select>
+            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
+        </div>
+
+        <!-- Address -->
+        <div class="mt-4">
+            <x-input-label for="address" :value="__('Address')" />
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
+            <x-input-error :messages="$errors->get('address')" class="mt-2" />
+        </div>
+
+        <!-- Date of Birth -->
+        <div class="mt-4">
+            <x-input-label for="dob" :value="__('Date Of Birth')" />
+            <x-text-input id="dob" class="block mt-1 w-full" type="date" name="dob" :value="old('dob')" required />
+            <x-input-error :messages="$errors->get('dob')" class="mt-2" />
+        </div>
+
+        <!-- Campus -->
+        <div class="mt-4">
+            <x-input-label for="campus" :value="__('Campus')" />
+            <x-text-input id="campus" class="block mt-1 w-full" type="text" name="campus" :value="old('campus')" required />
+            <x-input-error :messages="$errors->get('campus')" class="mt-2" />
+        </div>
+
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
@@ -19,23 +64,14 @@
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
